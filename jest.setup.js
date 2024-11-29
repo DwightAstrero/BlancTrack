fa06afa7cff2fa48577f775a1a77e3284a11b250
luis miguel <luis_robles@dlsu.edu.ph>
2024-11-29 16:45:36 +0800

    fixed paths, tests should work

---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+// Mock the environment variables for Supabase
+process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://hlzwuorawfdqbxdxbwik.supabase.co';  
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhsend1b3Jhd2ZkcWJ4ZHhid2lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2MDg3NzIsImV4cCI6MjAzOTE4NDc3Mn0.XYHXwxNmqIM_QCkVmuOlBood_-gGlbvKqlMwmpKfxho'; 
+
+
